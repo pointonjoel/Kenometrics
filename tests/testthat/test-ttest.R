5ce1a727ba7b4121669bca1ebe7d89d3f84b7e62
pointonjoel <pointon.joel@gmail.com>
2021-01-18 21:30:59 +0000

    ttest
    Updated function and tests

--- tests/testthat/test-ttest.R
+++ tests/testthat/test-ttest.R
@@ -7,7 +7,7 @@ test_that("T-test with 0 mean", {
   linear_model <- lm(linv ~ lpop + lprice, data=data)
   result <- ttest("lpop", data, linear_model)
   expect_equal(result,
-               "The t-value for lpop is 6.624, and the p-value is 3.469e-07."
+               "The t-value for lpop is 6.624, and the p-value is 0.0000."
   )
 })
 
@@ -15,6 +15,6 @@ test_that("T-test with non-0 mean", {
   linear_model <- lm(linv ~ lpop + lprice, data=data)
   result <- ttest("lpop", data, linear_model, -1)
   expect_equal(result,
-               "The t-value for lpop is 10.69, and the p-value is 2.148e-11."
+               "The t-value for lpop is 10.69, and the p-value is 0.0000."
   )
 })
