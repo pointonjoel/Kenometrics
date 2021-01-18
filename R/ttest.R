f4655f04f34dc150b00acd31cc389de4e57721be
pointonjoel <pointon.joel@gmail.com>
2021-01-18 20:36:30 +0000

    Update ttest.R

--- R/ttest.R
+++ R/ttest.R
@@ -26,24 +26,26 @@
 #' }
 ttest <-
   function (var_name, df, model, pop_mean=0){
+
     #Getting df name
     df.name <- deparse(substitute(my_data))
 
     #Getting the exact variable name used in the model
-    try2 <- function(code, silent = FALSE) {
-      fixed_name <- tryCatch(code, error = function(c) {
+    try2 <- function(code, silent = FALSE) { #I might need to pass in var_name
+      fixed_name <- tryCatch(code, error = function(c) { #same here
         if (!silent) {name <-
                       paste(df.name,"$",var_name, sep="")}
         else{name <- var_name}})
       return(fixed_name)}
 
     #Assigning variable name and continuing
-    correct_var_name <- try2(summary(model)$coefficients[var_name , 1])
+    correct_var_name <- try2(var_name)
     estimate <- summary(model)$coefficients[correct_var_name , 1]
     se <- summary(model)$coefficients[correct_var_name , 2]
     tstat <- (estimate-pop_mean)/se
     p_value <- 2*stats::pt(-abs(tstat), df=nrow(df)-ncol(df))
     return(print(paste("The t-value for ", var_name, " is ",
                        signif(tstat, digits = 4), ", and the p-value is ",
-                       signif(p_value, digits = 3), ".", sep = "")))
+                       formatC(signif(p_value, digits = 3),
+                        format = "f",digits = 4), ".", sep = "")))
   }
